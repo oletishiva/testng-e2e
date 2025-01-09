754060b6d0d383516deb01cdf6022f6a0bbd2327
oletishiva <oletishiva@gmail.com>
2025-01-09 23:01:27 +0530

    changes to readme and LoginTest files

--- README.md
+++ README.md
@@ -25,3 +25,25 @@ If you created a local repository, connect it to the remote GitHub repository: <
 git remote add origin https://github.com/username/my-project.git <br>
 Verify the connection: <br>
 git remote -v <br>
+
+
+### STEP 4
+#### Set Up a Slack Incoming Webhook
+Go to Slack Apps:
+
+Visit the Slack API page for creating webhooks.
+Create an App:
+
+Click Create New App.
+Select From Scratch.
+Name your app (e.g., "CircleCI Notifications") and choose your Slack workspace.
+Enable Webhooks:
+
+Under Features, go to Incoming Webhooks.
+Turn on Activate Incoming Webhooks.
+Add a Webhook to a Channel:
+
+Click Add New Webhook to Workspace.
+Select the channel where you want to receive notifications (e.g., #ci-notifications).
+Click Allow.
+Copy the Webhook URL (e.g., https://hooks.slack.com/services/XXXX/YYYY/ZZZZ).
--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -7,6 +7,11 @@ public class LoginTests {
         System.out.println("LoginTests - BeforeClass: Setting up preconditions for login tests.");
     }
 
+    @BeforeMethod
+    public void testBeforeMethod() {
+        System.out.println("LoginTests - Before Method: Setting up log  functionality.");
+    }
+
     @Test
     public void testValidLogin() {
         System.out.println("Executing LoginTests - Test: Valid Login Test.");
@@ -20,5 +25,25 @@ public class LoginTests {
     @AfterClass
     public void tearDownLogin() {
         System.out.println("LoginTests - AfterClass: Cleaning up after login tests.");
+    }
+
+    @AfterMethod
+    public void teardownLog() {
+        System.out.println("LoginTests - AfterMethod: teardown log after method.");
+    }
+
+    @BeforeTest
+    public void checkNetworkSignal() {
+        System.out.println("BeforeTest - check network signal.");
+    }
+
+    @AfterTest
+    public void turnOffSignal() {
+        System.out.println("AfterTest - turn off wifi network signal.");
+    }
+
+    @BeforeSuite
+    public void installAPk() {
+        System.out.println("Beforesuite - installApk.");
     }
 }
